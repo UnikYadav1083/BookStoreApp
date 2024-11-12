eb0dbe68e356369ca2d84057be16643e2b3d0825
Unik Yadav <132219783+UnikYadav1083@users.noreply.github.com>
2024-11-12 22:43:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Form from './components/form/Form';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+function App() {
+  return (
+    <div className='App'>
+    <Header/>
+    <Form/>
+    <Footer/>
+    </div>
+  );
+}
+ 
+export default App;
